3aa178eee5a92d565da208de29e42ddf2f9e3d4f
Swati Singhi <singhiswati11@gmail.com>
2016-05-21 12:31:21 +0530

    signin done

--- application/controllers/Landing_page.php
+++ application/controllers/Landing_page.php
@@ -11,11 +11,37 @@ public function signin()
 		$this->load->view('signin');
 	}
 public function librarian()
-{
-	$this->load->view('librarian');
-}
+	{
+		$this->load->view('librarian');
+	}
 public function add()
 	{
 		$this->load->view('add');
 	}
+
+public function authentication()
+	{
+		$signin = array('username'=>$this->input->post('username'),'password'=>$this->input->post('password'));
+		$entered_username = $signin["username"];
+		$entered_password = $signin["password"];
+		if($signin["username"]=="jecrc@lib")
+		{
+			if($signin["password"]=="123")
+			{
+				$success=1;
+			}
+			else
+			{
+				$success=0;
+			}
+		}
+		else
+		{			
+			$success=-1;
+		}	
+		$success=array("success"=>$success);
+		echo json_encode($success);
+	}
+
+
 }
--- assets/javascripts/signin.js
+++ assets/javascripts/signin.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
-	console.log("skjhfkljhglkj");
+	console.log("sign in page loaded");
 	var base_url = window.location.origin;
-	// var post_url_localhost = base_url+"/Library_management/index.php/Landing_page/librarian";
+	var post_url_localhost = base_url+"/Library_management/index.php/Landing_page/authentication";
 	// var post_url_openshift = base_url+"/index.php/Landing_page/authentication";
 	
 	$("#signinform").submit(function(){
@@ -11,25 +11,62 @@ $(document).ready(function(){
 	// Storing signIn data in signIn credentials.
 		var signin_credentials = {username:username,password:password,remember:checkbox};
 
-		if(username==="jecrc@lib")
-		{
-			console.log("hey");
-			if(password==="123")
-			{
-				console.log("i am");
-				$("button").bind("click", function(){
-					window.open("librarian","_self");					
-				});
-			}
-			else
-			{
-				alert("Incorrect Password !!!");
-			}
-		}
-		else
-		{
-			alert("Incorrect Username !!!");
-		}
+		$.ajax({
+			    type: 'POST',
+			    url: post_url_localhost,
+			    data: signin_credentials,
+			    dataType: "json",
+
+
+			    success : function(signin_result){
+			    	console.log(signin_result);
+			    	console.log(signin_result.success);
+
+			    	// var uuid=signin_result.uuid;
+			    	if(signin_result.success == 1){
+				    	console.log("successfully signed in!!!!!!!!!!!!!!");
+				    	window.open("librarian","_self");
+				    }
+				    else if(signin_result.success == 0){
+				    	alert("Incorrect password!!!");
+				    }
+				    else{
+				    	alert("Incorrect username !!!")
+				    }
+			    },
+			    error : function(){
+			        console.log("Signin Call failed !!!")
+			    }
+			});
+		event.preventDefault();
+
+
+
+
+
+
+
+
+
+		// if(username==="jecrc@lib")
+		// {
+		// 	console.log("hey");
+		// 	if(password==="123")
+		// 	{
+		// 		console.log("i am");
+		// 		$("#button").bind("click", function(){
+		// 			window.open("Landing_page/librarian","_self");					
+		// 		});
+		// 	}
+		// 	else
+		// 	{
+		// 		alert("Incorrect Password !!!");
+		// 	}
+		// }
+		// else
+		// {
+		// 	alert("Incorrect Username !!!");
+		// }
 	});
 
 });
